35e418c61d6bf14722332f1e72bedb0d2ea43fd5
Martha Conway <marthaconway93@gmail.com>
2017-12-21 15:05:58 +0000

    Update api_spec.rb

--- TechTest-master/test/spec/api_spec.rb
+++ TechTest-master/test/spec/api_spec.rb
@@ -5,7 +5,7 @@ describe "Devices" do
 	context "When requesting the notes for device with name 'ct'" do
 		it "Returns the notes 'Legacy Legato CT transmitter'" do
 			devices = ApiCall.new
-			expect(devices.notes('ct')).to eql("Legacy Legato CT transmitter")
+			expect(devices.notes('ct').values[0]).to eql("Legacy Legato CT transmitter")
 		end
 	end
 
